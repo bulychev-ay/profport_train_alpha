145624f0280e50a1b57bef52dadd90faddf92832
deployer <stasonjigg@gmail.com>
2017-12-23 15:26:15 +0500

    Вывод одной компетенции, добавили форму для создания новой компетенции

--- app/controllers/competences_controller.rb
+++ app/controllers/competences_controller.rb
@@ -2,4 +2,22 @@ class CompetencesController < ApplicationController
   def index
     @competences = Competence.all
   end
+
+  def show
+    @competence = Competence.find(params[:id])
+  end
+
+  def new
+    @competence = Competence.new
+  end
+
+  def create
+    @competence = Competence.create(competence_params)
+     render plain: 'Ok'
+  end
+
+  private
+     def competence_params
+       params.require(:competence).permit(:title, :description)
+     end
 end
